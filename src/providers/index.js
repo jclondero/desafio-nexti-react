bba85b6b2f5fbc8355b4af9a2f6394620602ce3a
Jean Carlos Londero <jean.londero@codengage.com>
2021-03-06 11:57:18 -0300

    feat(providers): adiciona providers de autenticacao

--- src/providers/index.js
+++ src/providers/index.js
@@ -1,11 +1,22 @@
-import React from 'react'
+import React, { Suspense } from 'react'
 
 import { I18nextProvider } from 'react-i18next'
 
 import i18n from './i18n'
+import Auth from './auth'
+
+import { ReactQueryProvider } from './reactQuery'
 
 const Providers = ({ children }) => {
-  return <I18nextProvider i18n={i18n}>{children}</I18nextProvider>
+  return (
+    <I18nextProvider i18n={i18n}>
+      <ReactQueryProvider>
+        <Suspense fallback={() => <p>Carregando...</p>}>
+          <Auth>{children}</Auth>
+        </Suspense>
+      </ReactQueryProvider>
+    </I18nextProvider>
+  )
 }
 
 export default Providers
