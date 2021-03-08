c6f1ee018e2da0d44d977d06083715998a32687d
Jean Carlos Londero <jean.londero@codengage.com>
2021-03-07 21:18:54 -0300

    feat(helpers): adiciona getCapitalize

--- src/helpers/dates.js
+++ src/helpers/dates.js
@@ -1,9 +1,11 @@
 import moment from 'moment'
 
+import { getCapitalize } from './strings'
+
 export const formatDate = (dateTime) => {
   if (moment(dateTime).isValid) {
     if (moment(dateTime).isSame(moment(), 'day')) {
-      return moment(dateTime).format('dddd, HH:mm')
+      return getCapitalize(moment(dateTime).format('dddd, HH:mm'))
     }
 
     return moment(dateTime).format('L')
--- src/helpers/strings.js
+++ src/helpers/strings.js
@@ -1,3 +1,8 @@
+export const getCapitalize = (string) => {
+  if (typeof string !== 'string') return ''
+  return string.charAt(0).toUpperCase() + string.slice(1)
+}
+
 export const getInitials = (string) => {
   return string
     .match(/(\b\S)?/g)
