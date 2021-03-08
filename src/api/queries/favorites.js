a42399aacac6f90965d2a77d338337a202915c09
Jean Carlos Londero <jean.londero@codengage.com>
2021-03-07 21:17:30 -0300

    refactor(api): ajusta retorno queryFavorites

--- src/api/queries/favorites.js
+++ src/api/queries/favorites.js
@@ -3,11 +3,31 @@ import { useQuery } from 'react-query'
 // Tive problemas de CORS ao consumir dados da API
 import db from '../db.json'
 
-export const queryFavorites = () => {
+export const queryFavorites = ({ archivedItems = [] }) => {
   return new Promise((resolve) => {
-    setTimeout(() => resolve(db.favorites), 1000)
+    if (archivedItems.length) {
+      setTimeout(
+        () =>
+          resolve(
+            db.favorites.map((favorite) => {
+              return {
+                ...favorite,
+                subMenus: favorite.subMenus.filter((subItem) => {
+                  const item = archivedItems.find((archivedItem) => archivedItem === subItem.id)
+
+                  return !item
+                }),
+              }
+            })
+          ),
+        1000
+      )
+    } else {
+      setTimeout(() => resolve(db.favorites), 1000)
+    }
   })
 }
 
-export const useQueryFavorites = (config) => useQuery('favorites', queryFavorites, config)
+export const useQueryFavorites = (queryParams, config) =>
+  useQuery('favorites', () => queryFavorites(queryParams), config)
 useQueryFavorites.queryKey = 'favorites'
